ebc6a5c3117eaa288dc9e0ae4480de0973745cb4
zbw <zbw@zbwdeMacBook-Pro.local>
2014-03-29 17:45:31 +0800

    op

---
+++ ww/ww/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ww
+//
+//  Created by zbw on 14-3-29.
+//  Copyright (c) 2014年 zbw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
