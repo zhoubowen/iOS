deca314cf3b9d36472d835f8b542b814979f025e
zbw <zbw@zbwdeMacBook-Pro.local>
2014-04-02 23:09:27 +0800

    IBAction 相当于 方法返回值为 void IBOutlet 可以设置属性跟故事面板中控件连接

---
+++ 计算器/计算器/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  计算器
+//
+//  Created by zbw on 14-4-2.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
